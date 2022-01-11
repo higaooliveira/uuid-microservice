70f89b9cc0510ef4cb41bae343a5ea61ec959cb1
Higor <higoroliveiiira@gmail.com>
2022-01-11 13:22:24 -0300

    Finishing lambda configuration + dynamo + api gateway

---
+++ test/uuid-microservice.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UuidMicroservice from '../lib/uuid-microservice-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/uuid-microservice-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UuidMicroservice.UuidMicroserviceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
